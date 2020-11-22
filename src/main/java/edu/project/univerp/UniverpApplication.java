ddc044fefde7b2ef68c5373a345f0f5f3184320f
hgxey6 <hgxey6@gmail.com>
2020-11-22 15:32:11 +0500

    init

---
+++ src/main/java/edu/project/univerp/UniverpApplication.java
@@ -0,0 +1,13 @@
+package edu.project.univerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniverpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/project/univerp/UniverpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.project.univerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
