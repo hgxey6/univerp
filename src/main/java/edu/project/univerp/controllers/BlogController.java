842ae21a7b65ba32d87f20ec947e955df71fbbb5
hgxey6 <hgxey6@gmail.com>
2020-11-26 07:12:58 +0500

    Просмотр, редактирование, удаление статьи

--- src/main/java/edu/project/univerp/controllers/BlogController.java
+++ src/main/java/edu/project/univerp/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -33,6 +37,56 @@ public class BlogController {
             Model model) {
         Article article = new Article(title, anons, text);
         articleRepo.save(article); //Сохраняет в репозитории объект
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogReadArticle(@PathVariable(value = "id") long id, Model model) {
+        if (articleRepo.existsById(id) == false) {
+            return "redirect:/blog";
+        }
+
+        Optional<Article> byId = articleRepo.findById(id);
+        ArrayList<Article> list = new ArrayList<>();
+        byId.ifPresent(list::add);
+        model.addAttribute("article", list);
+        return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogArticleEdit(@PathVariable(value = "id") long id, Model model) {
+        if (articleRepo.existsById(id) == false) {
+            return "redirect:/blog";
+        }
+
+        Optional<Article> byId = articleRepo.findById(id);
+        ArrayList<Article> list = new ArrayList<>();
+        byId.ifPresent(list::add);
+        model.addAttribute("article", list);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(
+            @PathVariable(value = "id") long id, @RequestParam String title,
+            @RequestParam String anons, @RequestParam String text, Model model) {
+
+        Article article = articleRepo.findById(id).orElseThrow();
+        article.setTitle(title);
+        article.setAnons(anons);
+        article.setText(text);
+
+        articleRepo.save(article);
+
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/delete")
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model) {
+
+        Article article = articleRepo.findById(id).orElseThrow();
+        articleRepo.delete(article);
+
         return "redirect:/blog";
     }
 }
